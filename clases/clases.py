500ed3c43bd52a389f05526d125c7427c6e44c5c
cajo <camiloduran2195@outlook.com>
2023-10-30 13:20:28 -0500

    cambios: promedio, total, y max en potencia activa

--- clases/clases.py
+++ clases/clases.py
@@ -3,74 +3,70 @@ import datetime
 import time
 
 class lectura():
-
     def potenciaActiva(self, selector ):
-        acutal = datetime.datetime.now().strftime("%Y-%m-%d")
-        listaPotenciaActivaL1 = []
-        listaPotenciaActivaL2 = []
-        listaPotenciaActivaL3 = []
+        try:
+            acutal = datetime.datetime.now().strftime("%Y-%m-%d")
+            listaPotenciaActivaL1 = []
+            listaPotenciaActivaL2 = []
+            listaPotenciaActivaL3 = []
 
-        if selector == True:
-            #potenciaActiva = db.session.query(potencia_activa_m1).order_by(db.session.fecha_hora.desc()).first()
-            potenciaActiva = potencia_activa_m1.query.order_by(potencia_activa_m1.fecha_hora.desc()).first()
-            potenciaActivaPromedio = potencia_activa_m1.query.order_by(potencia_activa_m1.fecha_hora.desc()) #1
+            if selector == True:
+                #potenciaActiva = db.session.query(potencia_activa_m1).order_by(db.session.fecha_hora.desc()).first()
+                potenciaActiva = potencia_activa_m1.query.order_by(potencia_activa_m1.fecha_hora.desc()).first()
+                potenciaActivaPromedio = potencia_activa_m1.query.order_by(potencia_activa_m1.fecha_hora.desc()) #1
 
-        elif selector == False:
-            potenciaActiva = potencia_activa_m2.query.order_by(potencia_activa_m2.fecha_hora.desc()).first()
-            potenciaActivaPromedio = db.session.query(potencia_activa_m2).all() #2
-            
-            #1 y 2 deben ser lista pero no all si no limitadas al día actual.
-        
-        promedioPotenciaActivaL1 = promedioPotenciaActivaL2 = promedioPotenciaActivaL3 = 0
-        totalPotenciaActivaL1 = totalPotenciaActivaL2 = totalPotenciaActivaL3 = 0
-        for potencia in potenciaActivaPromedio: 
-            potencia_activaL1 = potencia.pot_ac_L1
-            potencia_activaL2 = potencia.pot_ac_L2
-            potencia_activaL3 = potencia.pot_ac_L3
-            fecha_hora        = str(potencia.fecha_hora)
-            #print(fecha_hora[0:10])
-
-            if fecha_hora[0:10] == str(acutal):
-                listaPotenciaActivaL1.append(potencia_activaL1)
-                listaPotenciaActivaL2.append(potencia_activaL2)
-                listaPotenciaActivaL3.append(potencia_activaL3)
+            elif selector == False:
+                potenciaActiva = potencia_activa_m2.query.order_by(potencia_activa_m2.fecha_hora.desc()).first()
+                potenciaActivaPromedio = db.session.query(potencia_activa_m2).all() #2
                 
-        for valor1, valor2, valor3 in zip(listaPotenciaActivaL1, listaPotenciaActivaL2, listaPotenciaActivaL3):
-            promedioPotenciaActivaL1 += valor1/len(listaPotenciaActivaL1)
-            promedioPotenciaActivaL2 += valor2/len(listaPotenciaActivaL2)
-            promedioPotenciaActivaL3 += valor3/len(listaPotenciaActivaL3)
-            promedioPotenciaActivaL1 = round(promedioPotenciaActivaL1, 2)
-            promedioPotenciaActivaL2 = round(promedioPotenciaActivaL2, 2)
-            promedioPotenciaActivaL3 = round(promedioPotenciaActivaL3, 2)#esto se puede simplificar.
-            totalPotenciaActivaL1    += round(valor1, 2)
-            totalPotenciaActivaL2    += round(valor2, 2)
-            totalPotenciaActivaL3    += round(valor3, 2)
-        print(totalPotenciaActivaL1)
-        print(totalPotenciaActivaL2)
-        print(totalPotenciaActivaL3)    
-        potencia_activaL1 = potenciaActiva.pot_ac_L1
-        potencia_activaL2 = potenciaActiva.pot_ac_L2
-        potencia_activaL3 = potenciaActiva.pot_ac_L3
-        fecha_hora        = potenciaActiva.fecha_hora
-        maxL1             = max(listaPotenciaActivaL1)
-        maxL2             = max(listaPotenciaActivaL2)
-        maxL3             = max(listaPotenciaActivaL3)
+                #1 y 2 deben ser lista pero no all si no limitadas al día actual.
+            
+            promedioPotenciaActivaL1 = promedioPotenciaActivaL2 = promedioPotenciaActivaL3 = 0
+            totalPotenciaActivaL1 = totalPotenciaActivaL2 = totalPotenciaActivaL3 = 0
+            for potencia in potenciaActivaPromedio: 
+                potencia_activaL1 = potencia.pot_ac_L1
+                potencia_activaL2 = potencia.pot_ac_L2
+                potencia_activaL3 = potencia.pot_ac_L3
+                fecha_hora        = str(potencia.fecha_hora)
+                #print(fecha_hora[0:10])
 
-        potenciaActiva = {'potencia activa L1' : potencia_activaL1, 
-                          'potencia activa L2' : potencia_activaL2,
-                          'potencia activa L3' : potencia_activaL3,
-                          'Promedio L1'        : promedioPotenciaActivaL1,
-                          'Promedio L2'        : promedioPotenciaActivaL2,
-                          'Promedio L3'        : promedioPotenciaActivaL3,
-                          'Consumo mas alto L1': maxL1,
-                          'Consumo mas alto L2': maxL2,
-                          'Consumo mas alto L3': maxL3,
-                          'Consumo total L1'   : totalPotenciaActivaL1,
-                          'Consumo total L2'   : totalPotenciaActivaL2,
-                          'Consumo total L3'   : totalPotenciaActivaL3,
-                          'fecha_hora'         : fecha_hora}
-        
-        return potenciaActiva
+                if fecha_hora[0:10] == str(acutal):
+                    listaPotenciaActivaL1.append(potencia_activaL1)
+                    listaPotenciaActivaL2.append(potencia_activaL2)
+                    listaPotenciaActivaL3.append(potencia_activaL3)
+                    
+            for valor1, valor2, valor3 in zip(listaPotenciaActivaL1, listaPotenciaActivaL2, listaPotenciaActivaL3):
+                promedioPotenciaActivaL1 += valor1/len(listaPotenciaActivaL1)
+                promedioPotenciaActivaL2 += valor2/len(listaPotenciaActivaL2)
+                promedioPotenciaActivaL3 += valor3/len(listaPotenciaActivaL3)
+                totalPotenciaActivaL1    += valor1
+                totalPotenciaActivaL2    += valor2
+                totalPotenciaActivaL3    += valor3
+
+            potencia_activaL1 = potenciaActiva.pot_ac_L1
+            potencia_activaL2 = potenciaActiva.pot_ac_L2
+            potencia_activaL3 = potenciaActiva.pot_ac_L3
+            fecha_hora        = potenciaActiva.fecha_hora
+            maxL1             = max(listaPotenciaActivaL1)
+            maxL2             = max(listaPotenciaActivaL2)
+            maxL3             = max(listaPotenciaActivaL3)
+
+            potenciaActiva = {'activa_L1': potencia_activaL1, 
+                            'activa_L2'  : potencia_activaL2,
+                            'activa_L3'  : potencia_activaL3,
+                            'Promedio_L1': round(promedioPotenciaActivaL1, 2),
+                            'Promedio_L2': round(promedioPotenciaActivaL2, 2),
+                            'Promedio_L3': round(promedioPotenciaActivaL3, 2),
+                            'highest_L1' : maxL1,
+                            'highest_L2' : maxL2,
+                            'highest_L3' : maxL3,
+                            'total_L1'   : round(totalPotenciaActivaL1, 2),
+                            'total_L2'   : round(totalPotenciaActivaL2, 2),
+                            'total_L3'   : round(totalPotenciaActivaL3, 2),
+                            'date'       : fecha_hora}
+            return potenciaActiva
+        except:
+            return 0
     
     def potenciaReactiva(self, selector ):
         if selector == True:
@@ -84,10 +80,10 @@ class lectura():
         potencia_reactivaL3 = potenciaReactiva.pot_rea_L3
         fecha_hora          = potenciaReactiva.fecha_hora
 
-        potenciaActiva = {'potencia Reactiva L1' : potencia_reactivaL1, 
-                          'potencia Reactiva L2' : potencia_reactivaL2,
-                          'potencia Reactiva L3' : potencia_reactivaL3,
-                          'fecha_hora'           : fecha_hora}
+        potenciaActiva = {'reactiva_L1' : potencia_reactivaL1, 
+                          'reactiva_L2' : potencia_reactivaL2,
+                          'reactiva_L3' : potencia_reactivaL3,
+                          'fecha_hora'          : fecha_hora}
         
         return potenciaActiva
     
@@ -103,10 +99,10 @@ class lectura():
         factorDePotenciaL3 = factorDePotencia.fac_pot_L3
         fecha_hora         = factorDePotencia.fecha_hora
 
-        factorDePotencia = {'Factor de Potencia L1' : factorDePotenciaL1, 
-                            'Factor de Potencia L2' : factorDePotenciaL2,
-                            'Factor de Potencia L3' : factorDePotenciaL3,
-                            'fecha_hora'            : fecha_hora}
+        factorDePotencia = {'FP_L1'     : factorDePotenciaL1, 
+                            'FP_L2'     : factorDePotenciaL2,
+                            'FP_L3'     : factorDePotenciaL3,
+                            'fecha_hora': fecha_hora}
         
         return factorDePotencia
     # Entiendes este fragmento de codigo?    
@@ -122,9 +118,9 @@ class lectura():
         voltajeEntreL3yL1  = voltajeEntreLineas.tension_L3_L1
         fecha_hora         = voltajeEntreLineas.fecha_hora
 
-        voltajeEntreLineas = {'Tension entre L1yL2' : voltajeEntreL1yL2, 
-                              'Tension entre L2yL3' : voltajeEntreL2yL3,
-                              'Tension entre L3yL1' : voltajeEntreL3yL1,
+        voltajeEntreLineas = {'tension_L1L2' : voltajeEntreL1yL2, 
+                              'tension_L2L3' : voltajeEntreL2yL3,
+                              'tension_L3L1' : voltajeEntreL3yL1,
                               'fecha_hora'          : fecha_hora}
         
         return voltajeEntreLineas
@@ -146,9 +142,9 @@ class lectura():
         corriente_L3 = corrientesFase.corriente_L3
         fecha_hora   = corrientesFase.fecha_hora
 
-        corriente = {'Corriente L1' : corriente_L1, 
-                     'Corriente L2' : corriente_L2,
-                     'Corriente L3' : corriente_L3,
+        corriente = {'corriente_L1' : corriente_L1, 
+                     'corriente_L2' : corriente_L2,
+                     'corriente_L3' : corriente_L3,
                      'fecha_hora'   : fecha_hora}
 
         return corriente
